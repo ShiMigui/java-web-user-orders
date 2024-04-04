ff814cac039d8c3d2bb36e464f856f6d9c7c322d
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-04 18:16:58 -0300

    Added: User entity and resource

---
+++ src/main/java/com/shimigui/WebServices/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.shimigui.WebServices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shimigui.WebServices.entities.User;
+
+@RestController
+@RequestMapping(value="/user")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "Miguel Nascimento", "miguel@gmail.com", "+5513996722405", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
