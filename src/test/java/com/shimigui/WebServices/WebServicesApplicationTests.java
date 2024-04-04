d9493e4bcfa161f9ea788db0fb3b92c3a7aa029e
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-04 17:57:53 -0300

    Init: A project with Spring initializr

---
+++ src/test/java/com/shimigui/WebServices/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimigui.WebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
