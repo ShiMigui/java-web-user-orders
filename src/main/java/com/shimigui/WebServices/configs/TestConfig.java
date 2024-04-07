c6202b1fd53aa720a8fc3978590cbe8738bea1d0
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-07 20:41:56 -0300

    Order payment added and association one-to-one

--- src/main/java/com/shimigui/WebServices/configs/TestConfig.java
+++ src/main/java/com/shimigui/WebServices/configs/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.shimigui.WebServices.entities.Category;
 import com.shimigui.WebServices.entities.Order;
 import com.shimigui.WebServices.entities.OrderItem;
+import com.shimigui.WebServices.entities.Payment;
 import com.shimigui.WebServices.entities.Product;
 import com.shimigui.WebServices.entities.User;
 import com.shimigui.WebServices.entities.enums.OrderStatus;
@@ -34,7 +35,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository products;
-	
+
 	@Autowired
 	private OrderItemRepository orderItems;
 
@@ -44,13 +45,13 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		categories.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-		
+
 		p1.addCategory(cat2);
 		p2.addCategory(cat1);
 		p2.addCategory(cat3);
@@ -58,20 +59,22 @@ public class TestConfig implements CommandLineRunner {
 		p4.addCategory(cat3);
 		p5.addCategory(cat2);
 		products.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		User u1 = new User("Miguel Nascimento dos Santos", "miguel.nasto@gmail.com", "13996722405", "12345");
 		User u2 = new User("Manuela D'ávilla", "manuela@gmail.com", "13999999999", "12345");
 		users.saveAll(Arrays.asList(u1, u2));
-		
+
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+		o1.setPayment(new Payment(null, Instant.parse("2019-06-20T19:53:28Z"), o1));
 		orders.saveAll(Arrays.asList(o1, o2, o3));
-		
-		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
-		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
-		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
-		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItems.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
