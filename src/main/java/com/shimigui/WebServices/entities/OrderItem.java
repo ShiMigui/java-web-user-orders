dfec0653cf17161941e2f9a10a0d6fe9df602678
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-07 19:41:51 -0300

    OrderitemPK and OrderItem basic class

---
+++ src/main/java/com/shimigui/WebServices/entities/OrderItem.java
@@ -0,0 +1,69 @@
+package com.shimigui.WebServices.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.shimigui.WebServices.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private OrderItemPK id;
+	private Integer quantity;
+	private Double price;
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		this.id = new OrderItemPK(product, order);
+		setQuantity(quantity);
+		setPrice(price);
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/com/shimigui/WebServices/entities/pk/OrderItemPK.java
@@ -0,0 +1,59 @@
+package com.shimigui.WebServices.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.shimigui.WebServices.entities.Order;
+import com.shimigui.WebServices.entities.Product;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public OrderItemPK(Product product, Order order) {
+		this.product = product;
+		this.order = order;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+}
