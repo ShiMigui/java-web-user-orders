5ce90e5fc6d7c852aa8ce3b56dd046c8af908ba1
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-14 20:40:33 -0300

    Update: Service and resource relative to User, update() method

--- src/main/java/com/shimigui/WebServices/services/UserService.java
+++ src/main/java/com/shimigui/WebServices/services/UserService.java
@@ -28,4 +28,16 @@ public class UserService {
 	public void delete(Integer id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Integer id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
