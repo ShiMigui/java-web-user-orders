b5017e7c5eb9fcfc7d74b7c91971dca6def610a2
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-17 20:23:26 -0300

    method updateData(User, User) of UserService

--- src/main/java/com/shimigui/WebServices/services/UserService.java
+++ src/main/java/com/shimigui/WebServices/services/UserService.java
@@ -14,6 +14,18 @@ public class UserService extends EntityService<User> {
 
 	@Override
 	protected void updateData(User entity, User obj) {
+		String password = obj.getPassword();
+		String phone = obj.getPhone();
+		String email = obj.getEmail();
+		String name = obj.getName();
 
+		if (email != null)
+			entity.setEmail(email);
+		if (phone != null)
+			entity.setPhone(phone);
+		if (name != null)
+			entity.setName(name);
+		if (password != null)
+			entity.setPassword(password);
 	}
 }
