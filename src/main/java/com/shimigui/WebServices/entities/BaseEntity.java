b2cd3350face85356b51336de1307a62fcaea40c
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-17 16:34:28 -0300

    Added: Class BaseEntity, a superclass generic to classes

---
+++ src/main/java/com/shimigui/WebServices/entities/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.shimigui.WebServices.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity<G extends BaseEntity<G>> implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		G other = (G) obj;
+		return Objects.equals(id, other.id);
+	}
+}
